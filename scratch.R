9d8e54105c7f0db0636e2e6dd823ebb8b8ee4ac5
tarapozzi <tarapozzi@u.boisestate.edu>
2024-09-07 15:21:55 -0700

    overhaul

---
+++ scratch.R
@@ -0,0 +1,16 @@
+# Scratch code
+
+# From index.qmd
+about:
+  template: broadside
+image: images/IMG_0103.jpeg
+links:
+  - icon: twitter
+text: Twitter
+href: https://twitter.com/pozzi_tara
+- icon: github
+text: Github
+href: https://github.com/tarapozzi
+website: 
+  navbar: 
+  background: darkolivegreen
